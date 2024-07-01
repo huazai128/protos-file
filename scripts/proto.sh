0fc2b11fdd04ffeedcb6b3d1b98cf0c5e981a48a
zhouweihua <zhouweihua@joyy.com>
2024-07-01 21:43:42 +0800

    编译proto

---
+++ scripts/proto.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROTO_DIR="./"      # 相对路径指定 proto 文件路径
+OUTPUT_DIR="./"    # 相对路径指定输出目录
+
+PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts_proto"   # protoc-gen-ts_proto 插件路径
+
+# rm -rf "$OUTPUT_DIR"
+mkdir -p "$OUTPUT_DIR"
+
+protoc \
+  --plugin="protoc-gen-ts_proto=$PROTOC_GEN_TS_PATH" \
+  --ts_proto_out="$OUTPUT_DIR" \
+  --ts_proto_opt=esModuleInterop=true \
+  -I "$PROTO_DIR" \
+  "$PROTO_DIR"/*.proto
