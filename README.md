fc3b3cc8f24f7f0a6fe7bf4af9942ab2f1c7a1d9
zhouweihua <zhouweihua@joyy.com>
2024-07-02 17:03:07 +0800

    feat: 更新proto脚本

---
+++ README.md
@@ -0,0 +1,16 @@
+
+  
+## Description
+protos 项目 用于存储proto
+
+yarn tsp 和 yarn build:protos 运行都会报错
+
+目前只能在控制台中输入 tsproto --path ./protos
+
+## Installation
+
+```bash
+$ tsproto --path ./protos
+```
+
+
--- scripts/proto.sh
+++ scripts/proto.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-PROTO_DIR="./"      # 相对路径指定 proto 文件路径
-OUTPUT_DIR="./"    # 相对路径指定输出目录
+# PROTO_DIR="./"      # 相对路径指定 proto 文件路径
+# OUTPUT_DIR="./"    # 相对路径指定输出目录
 
-PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts_proto"   # protoc-gen-ts_proto 插件路径
+# PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts_proto"   # protoc-gen-ts_proto 插件路径
 
-# rm -rf "$OUTPUT_DIR"
-mkdir -p "$OUTPUT_DIR"
+# # rm -rf "$OUTPUT_DIR"
+# mkdir -p "$OUTPUT_DIR"
 
-protoc \
-  --plugin="protoc-gen-ts_proto=$PROTOC_GEN_TS_PATH" \
-  --ts_proto_out="$OUTPUT_DIR" \
-  --ts_proto_opt=esModuleInterop=true \
-  -I "$PROTO_DIR" \
-  "$PROTO_DIR"/*.proto
+# protoc \
+#   --plugin="protoc-gen-ts_proto=$PROTOC_GEN_TS_PATH" \
+#   --ts_proto_out="$OUTPUT_DIR" \
+#   --ts_proto_opt=esModuleInterop=true \
+#   -I "$PROTO_DIR" \
+#   "$PROTO_DIR"/*.proto
+
+tsproto --path ./protos
